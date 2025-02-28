3840e13117239f04266aacdc3e002d256670b23b
Derek Melchin <38889814+DerekMelchin@users.noreply.github.com>
2025-02-28 13:19:03 -0500

    Update to PEP8

--- CryptoCoarseFundamentalUniverseSelectionFrameworkAlgorithm.py
+++ CryptoCoarseFundamentalUniverseSelectionFrameworkAlgorithm.py
@@ -17,53 +17,56 @@ from AlgorithmImports import *
 ### Example algorithm using the custom data type as a source of alpha
 ### </summary>
 class CryptoCoarseFundamentalUniverseSelectionAlgorithm(QCAlgorithm): 
-    def Initialize(self):
+    
+
+    def initialize(self):
         ''' Initialise the data and resolution required, as well as the cash and start-end dates for your algorithm. All algorithms must initialized. '''
 
-        self.SetStartDate(2020, 6, 1)
-        self.SetEndDate(2020, 6, 5)
-        self.SetCash(100000)
+        self.set_start_date(2020, 6, 1)
+        self.set_end_date(2020, 6, 5)
+        self.set_cash(100000)
         
-        self.SetBrokerageModel(BrokerageName.Bitfinex, AccountType.Cash)
+        self.set_brokerage_model(BrokerageName.BITFINEX, AccountType.CASH)
         
         # Warm up the security with the last known price to avoid conversion error
-        self.SetSecurityInitializer(lambda security: security.SetMarketPrice(self.GetLastKnownPrice(security)));
+        self.set_security_initializer(lambda security: security.set_market_price(self.get_last_known_price(security)));
 
         # Data ADDED via universe selection is added with Daily resolution.
-        self.UniverseSettings.Resolution = Resolution.Daily
+        self.universe_settings.resolution = Resolution.DAILY
         # Add universe selection of cryptos based on coarse fundamentals
-        self.AddUniverseSelection(CoinbaseCoarseFundamentalUniverseSelectionModel(self.UniverseSettings))
+        self.add_universe_selection(CoinbaseCoarseFundamentalUniverseSelectionModel(self.universe_settings))
 
         # Alternatively, we can define the selector function in the algorithm class:
-        #self.AddUniverseSelection(CryptoCoarseFundamentalUniverseSelectionModel(Market.GDAX, self.UniverseSettings, self.UniverseSelectionFilter))
+        #self.add_universe_selection(CryptoCoarseFundamentalUniverseSelectionModel(Market.GDAX, self.universe_settings, self._universe_selection_filter))
         
 
-    def UniverseSelectionFilter(self, crypto_coarse):
+    def _universe_selection_filter(self, crypto_coarse):
         ''' Selected the securities
         
         :param List of CryptoCoarseFundamentalUniverse crypto_coarse: List of CryptoCoarseFundamentalUniverse
         :return: List of Symbol objects '''
         filtered = [datum for datum in crypto_coarse
-                if datum.Volume >= 100 
-                and datum.VolumeInUsd > 10000]
-        sorted_by_volume_in_usd = sorted(filtered, key=lambda datum: datum.VolumeInUsd, reverse=True)[:10]
+                if datum.volume >= 100 
+                and datum.volume_in_usd > 10000]
+        sorted_by_volume_in_usd = sorted(filtered, key=lambda datum: datum.volume_in_usd, reverse=True)[:10]
         
-        return [datum.Symbol for datum in sorted_by_volume_in_usd]
+        return [datum.symbol for datum in sorted_by_volume_in_usd]
 
-    def OnSecuritiesChanged(self, changes):
+    def on_securities_changed(self, changes):
         ''' Event fired each time that we add/remove securities from the data feed
 		
         :param SecurityChanges changes: Security additions/removals for this time step
         '''
-        self.Log(changes.ToString())
+        self.log(str(changes))
 
 
 class CoinbaseCoarseFundamentalUniverseSelectionModel(CryptoCoarseFundamentalUniverseSelectionModel):
+
     def __init__(self, universe_settings):
-        def Selector(crypto_coarse):
+        def selector(crypto_coarse):
             filtered = [datum for datum in crypto_coarse
-                if datum.Volume >= 100 
-                and datum.VolumeInUsd > 10000]
-            sorted_by_volume_in_usd = sorted(filtered, key=lambda datum: datum.VolumeInUsd, reverse=True)[:10]
-            return [datum.Symbol for datum in sorted_by_volume_in_usd]
-        super().__init__(Market.GDAX, universe_settings, Selector)
+                if datum.volume >= 100 
+                and datum.volume_in_usd > 10000]
+            sorted_by_volume_in_usd = sorted(filtered, key=lambda datum: datum.volume_in_usd, reverse=True)[:10]
+            return [datum.symbol for datum in sorted_by_volume_in_usd]
+        super().__init__(Market.GDAX, universe_settings, selector)
